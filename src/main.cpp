66ac20d297b6de7ee06e93359ddc8269ce82de72
Adriiix <adrrr2000@gmail.com>
2023-05-29 10:00:33 +0200

    Premiers essais interface graphique

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,13 @@ int main(int argc, char* argv[])
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::PinkPearl);
         ctx.fill = p6::NamedColor::OrangePeel;
-        ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.3f});
+        ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.1f});
         ctx.text_size = 0.1f;
         ctx.fill      = {1.f, 0.f, 0.f, 1.f};
-        ctx.text(u"abcdefghijklmnop", p6::Center{0.f, 2.f * ctx.text_size});
-        ctx.fill = {1.f, 0.f, 0.f, 0.5f};
-        ctx.text(u"qrstuvwxyz", p6::Center{0.f, 0.f * ctx.text_size});
-        ctx.text(u"12345 6789", p6::Center{0.f, -2.f * ctx.text_size});
-        ctx.text(u"!\\\"#$%&\'", p6::Center{0.f, -4.f * ctx.text_size});
-        ctx.text(u",()*+-./:;", p6::Center{0.f, -6.f * ctx.text_size});
-        ctx.text(u"<=>?@[]^_`{|}~", p6::Center{0.f, -8.f * ctx.text_size});
+        ctx.text(u"Pierre", p6::Center{0.f, 2.f * ctx.text_size});
+        ctx.fill = {1.f, 0.f, 0.f, 1.f};
+        ctx.text(u"Feuille", p6::Center{0.f, 0.f * ctx.text_size});
+        ctx.text(u"Ciseaux", p6::Center{0.f, -2.f * ctx.text_size});
     };
 
     // Should be done last. It starts the infinite loop.
