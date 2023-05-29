f02b039ad20ffce50058e935dc3dba5ebb49b6b1
Adriiix <adrrr2000@gmail.com>
2023-05-29 09:43:41 +0200

    Exemple d'affichage de texte avec p6

--- src/main.cpp
+++ src/main.cpp
@@ -8,63 +8,60 @@ int main(int argc, char* argv[])
     // Actual app
     auto ctx = p6::Context{{.title = "Pierre Feuille Ciseaux"}};
 
-    // P = 0, F = 1, C = 2
-    // int choixOrdinateur  = 0; // Choix de l'ordinateur
-    // int scoreUtilisateur = 0; // Score de l'utilisateur
-    // int scoreOrdinateur  = 0; // Score de l'ordinateur
-
-    // while (scoreUtilisateur < 3 && scoreOrdinateur < 3)
-    // {
-    //     int choixUtilisateur = demanderChoixUtilisateur();
-    //     choixOrdinateur      = (choixOrdinateur + 1) % 3;
-
-    //     afficherResultat(choixUtilisateur, choixOrdinateur);
-
-    //     if ((choixUtilisateur == 0 && choixOrdinateur == 2) || (choixUtilisateur == 1 && choixOrdinateur == 0) || (choixUtilisateur == 2 && choixOrdinateur == 1))
-    //     {
-    //         // L'utilisateur a gagné
-    //         scoreUtilisateur++;
-    //     }
-    //     else if ((choixUtilisateur == 2 && choixOrdinateur == 0) || (choixUtilisateur == 0 && choixOrdinateur == 1) || (choixUtilisateur == 1 && choixOrdinateur == 2))
-    //     {
-    //         // L'ordinateur a gagné
-    //         scoreOrdinateur++;
-    //     }
-
-    //     std::cout << "Score : Utilisateur " << scoreUtilisateur << " - "
-    //               << scoreOrdinateur << " Ordinateur" << std::endl;
-    // }
-
-    // if (scoreUtilisateur == 3)
-    // {
-    //     std::cout << "Vous avez gagné la partie !" << std::endl;
-    // }
-    // else
-    // {
-    //     std::cout << "L'ordinateur a gagné la partie !" << std::endl;
-    // }
-
     ctx.maximize_window();
-    ctx.imgui = [&]() {
-        // Show a simple window
-        // ImGui::Begin("Test");
-
-        // ImGui::SliderFloat("distance Between Boids", &DISTANCE, 0.001, 10);
-        // ImGui::SliderFloat("separation Strength", &(strengths.m_separationStrength), 0, 2);
-        // ImGui::SliderInt("cohesion Strength", &(strengths.m_cohesionStrength), 0, 10);
-        // ImGui::SliderInt("alignment Strength", &(strengths.m_alignmentStrength), 0, 10);
-        // ImGui::SliderFloat("maximum speed", &(strengths.m_vitesseMax), 0.001, 0.02);
-
-        // ImGui::End();
-        // Show the official ImGui demo window
-        // It is very useful to discover all the widgets available in ImGui
-    };
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
-        ctx.background({0.151 * 3, 0.253 * 3, 0.135 * 3});
+        ctx.background(p6::NamedColor::PinkPearl);
+        ctx.fill = p6::NamedColor::OrangePeel;
+        ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.3f});
+        ctx.text_size = 0.1f;
+        ctx.fill      = {1.f, 0.f, 0.f, 1.f};
+        ctx.text(u"abcdefghijklmnop", p6::Center{0.f, 2.f * ctx.text_size});
+        ctx.fill = {1.f, 0.f, 0.f, 0.5f};
+        ctx.text(u"qrstuvwxyz", p6::Center{0.f, 0.f * ctx.text_size});
+        ctx.text(u"12345 6789", p6::Center{0.f, -2.f * ctx.text_size});
+        ctx.text(u"!\\\"#$%&\'", p6::Center{0.f, -4.f * ctx.text_size});
+        ctx.text(u",()*+-./:;", p6::Center{0.f, -6.f * ctx.text_size});
+        ctx.text(u"<=>?@[]^_`{|}~", p6::Center{0.f, -8.f * ctx.text_size});
     };
 
     // Should be done last. It starts the infinite loop.
     ctx.start();
 }
+
+// P = 0, F = 1, C = 2
+// int choixOrdinateur  = 0; // Choix de l'ordinateur
+// int scoreUtilisateur = 0; // Score de l'utilisateur
+// int scoreOrdinateur  = 0; // Score de l'ordinateur
+
+// while (scoreUtilisateur < 3 && scoreOrdinateur < 3)
+// {
+//     int choixUtilisateur = demanderChoixUtilisateur();
+//     choixOrdinateur      = (choixOrdinateur + 1) % 3;
+
+//     afficherResultat(choixUtilisateur, choixOrdinateur);
+
+//     if ((choixUtilisateur == 0 && choixOrdinateur == 2) || (choixUtilisateur == 1 && choixOrdinateur == 0) || (choixUtilisateur == 2 && choixOrdinateur == 1))
+//     {
+//         // L'utilisateur a gagné
+//         scoreUtilisateur++;
+//     }
+//     else if ((choixUtilisateur == 2 && choixOrdinateur == 0) || (choixUtilisateur == 0 && choixOrdinateur == 1) || (choixUtilisateur == 1 && choixOrdinateur == 2))
+//     {
+//         // L'ordinateur a gagné
+//         scoreOrdinateur++;
+//     }
+
+//     std::cout << "Score : Utilisateur " << scoreUtilisateur << " - "
+//               << scoreOrdinateur << " Ordinateur" << std::endl;
+// }
+
+// if (scoreUtilisateur == 3)
+// {
+//     std::cout << "Vous avez gagné la partie !" << std::endl;
+// }
+// else
+// {
+//     std::cout << "L'ordinateur a gagné la partie !" << std::endl;
+// }
