9ee2cde1916bda959424006163d6e46d781f5d5b
Adriiix <adrrr2000@gmail.com>
2023-05-29 09:02:48 +0200

    premier à 3 points gagne

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,36 @@
 
 int main() {
   // P = 0, F = 1, C = 2
-  int choixOrdinateur = 0; // Choix de l'ordinateur
-  std::string jeu = "O";
+  int choixOrdinateur = 0;  // Choix de l'ordinateur
+  int scoreUtilisateur = 0; // Score de l'utilisateur
+  int scoreOrdinateur = 0;  // Score de l'ordinateur
 
-  while (jeu == "o" || jeu == "O") {
-
+  while (scoreUtilisateur < 3 && scoreOrdinateur < 3) {
     int choixUtilisateur = demanderChoixUtilisateur();
     choixOrdinateur = (choixOrdinateur + 1) % 3;
 
     afficherResultat(choixUtilisateur, choixOrdinateur);
 
-    std::cout << "Rejouer ? O/N" << std::endl;
-    std::cin >> jeu;
+    if ((choixUtilisateur == 0 && choixOrdinateur == 2) ||
+        (choixUtilisateur == 1 && choixOrdinateur == 0) ||
+        (choixUtilisateur == 2 && choixOrdinateur == 1)) {
+      // L'utilisateur a gagné
+      scoreUtilisateur++;
+    } else if ((choixUtilisateur == 2 && choixOrdinateur == 0) ||
+               (choixUtilisateur == 0 && choixOrdinateur == 1) ||
+               (choixUtilisateur == 1 && choixOrdinateur == 2)) {
+      // L'ordinateur a gagné
+      scoreOrdinateur++;
+    }
+
+    std::cout << "Score : Utilisateur " << scoreUtilisateur << " - "
+              << scoreOrdinateur << " Ordinateur" << std::endl;
+  }
+
+  if (scoreUtilisateur == 3) {
+    std::cout << "Vous avez gagné la partie !" << std::endl;
+  } else {
+    std::cout << "L'ordinateur a gagné la partie !" << std::endl;
   }
 
   return 0;
